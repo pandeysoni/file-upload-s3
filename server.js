7d845969d891b07b3864ffe5c7ba5b8b151d5ae4
Soni Pandey <sonipandey.71@gmail.com>
2016-05-09 18:03:11 +0530

    make it better

--- server.js
+++ server.js
@@ -1,27 +1,15 @@
 var express = require('express'),
     Routes = require('./server/routes'),
-    // Db = require('./server/config/db'),
     config = require('./server/config/config'),
     bodyParser = require('body-parser'),
-    path = require('path'),
-    expressSession = require('express-session'),
-	cookieParser = require('cookie-parser'),
     app = express();
 
-app.use(cookieParser());
-app.use(expressSession({secret:'somesecrettokenhere'}));
-app.use(express.static(path.join(__dirname, 'client/')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 
 /** load routes*/
-app.get('/', function(req, res) {
-    res.sendFile('./client/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-});
-app.get('/credential', function(req, res) {
-  res.json(req.session);
-});
+
 require('./server/routes')(app);
 
 var port = config.server.port;
--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,36 @@
-// API Server Endpoints
-module.exports = function(app){
-  require("./user/user.server.routes")(app);
+var nodemailer = require('nodemailer');
+ 
+// create reusable transporter object using the default SMTP transport 
+var transporter = nodemailer.createTransport({service: 'gmail',
+  auth: {
+    user: 'roy.dajshi@gmail.com',
+    pass: 'dajshi123'
+  }});
+ 
+// setup e-mail data with unicode symbols 
+var sendMail = function(data, res){
+	var mailOptions = {
+	    from: '"github.io 👥" <barmeshwar.1432@gmail.com>', // sender address 
+	    to: 'sonipandey.71@gmail.com', // list of receivers 
+	    subject: 'Hello ✔', // Subject line 
+	    html: "Hello,<br> You have received a mail from:<br>Name: "+data.Name +"<br>Email: "+ data.Email +"<br>Message: "+ data.Message+"<br>", // plaintext body 
+	}
+	transporter.sendMail(mailOptions, function(error, info){
+	    if(error){
+	        return console.log(error);
+	    }
+	    return res.send('success');
+	    console.log('Message sent: ' + info.response);
+	});
+};
+ 
+
+
+module.exports = function(app, passport){
+	// API Server Endpoints
+	app.post('/send', function(req, res){
+	  // send mail with defined transport object 
+	  sendMail(req.body, res);
+	})
 }
+
