3f82836cd3079097d3c0a55b49accc0bf45cbaaa
Soni Pandey <sonipandey.71@gmail.com>
2017-08-05 19:35:30 +0530

    Update routes.js

--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,7 @@
 const Upload = require('./upload/upload.server.controller')
 const multipart = require('connect-multiparty')
 const multipartMiddleware = multipart()
-const multer  = require('multer')
+
 module.exports = function(app){
 	// API Server Endpoints
 	app.get('/', Upload.displayForm)
